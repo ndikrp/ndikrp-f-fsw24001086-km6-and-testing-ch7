143b4a9749bdd9ebf6ba1dd612b0c53259418c88
ndikrp <andhikarizky36@gmail.com>
2024-05-17 00:58:59 +0700

    deploy to railway

--- config/database.js
+++ config/database.js
@@ -1,14 +1,10 @@
 const {
-  DB_USER = "postgres",
-  DB_PASSWORD = "admin",
-  DB_NAME = "ch7_dev",
-  DB_HOST = "localhost",
-  DB_PORT = "5432",
+  DATABASE_URL="postgresql://postgres:bieiFOuPaXQKdWEYtLyOPpgVzOhvFneT@monorail.proxy.rlwy.net:27861/railway"
 } = process.env;
 
 module.exports = {
   test: {
-    database: "postgresql://postgres:bieiFOuPaXQKdWEYtLyOPpgVzOhvFneT@monorail.proxy.rlwy.net:27861/railway",
+    database: DATABASE_URL,
     dialect: 'postgres'
   }
 }
